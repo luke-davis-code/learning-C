3b748c0eb509999181d200199e59c5ce0e1e4113
Luke Davis <lukeda117@gmail.com>
2025-02-04 15:00:57 +0000

    Level 1: Acquaintance, Chapter 3: Everything is about control, Sections 1 and 2

--- getting-started.c
+++ getting-started.c
@@ -14,10 +14,12 @@ int main (void ) {
 
     // Doing some work
     for (size_t i = 0; i < 5; ++i) {
-        printf ("element␣%zu␣is␣%g,␣\tits␣square␣is␣%g\n",
-                i,
-                A[i],
-                A[i]*A[i]);
+       if (i) {
+            printf ("element␣%zu␣is␣%g,␣\tits␣square␣is␣%g\n",
+                    i,
+                    A[i],
+                    A[i]*A[i]);
+       }
     }
 
     return EXIT_SUCCESS ;
