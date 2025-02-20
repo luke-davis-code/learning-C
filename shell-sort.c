b0825383a8f26e801f4a0ecd43e58edea786f0ea
Luke Davis <lukeda117@gmail.com>
2025-02-20 15:07:39 +0000

    Shell Sort

--- shell-sort.c
+++ shell-sort.c
@@ -1,5 +1,6 @@
 # include <stdio.h>
 # include <stdlib.h>
+# include <time.h>
 
 int power(int base, int exp){
     int result = 1;
@@ -9,23 +10,22 @@ int power(int base, int exp){
     return result;
 }
 
-
 void swap(int a[], int i, int j) {
     int temp = a[i];
     a[i] = a[j];
     a[j] = temp;
 }
 
-void insert(int arr[], int i){
-    while (i > 0 && arr[i-1] > arr[i]){
-        swap(arr, i-1, i);
-        i--;
+void hInsert(int arr[], int i, int h){
+    while (i >= h && arr[i-h] > arr[i]){
+        swap(arr, i-h, i);
+        i = i - h;
     }
 }
 
-void insertionSort(int arr[], int n) {
-    for (int i = 1; i < n; i++) {
-        insert(arr, i);
+void hSort(int arr[], int n, int h) {
+    for (int i = h; i < n; i++) {
+        hInsert(arr, i, h);
     }
 }
 
@@ -37,14 +37,28 @@ void shellSort(int arr[], int n) {
         stridelengths[k-1] = power(2, k)-1;
     }
 
-    for (int i = k; i >= 0; i--){
-        // Do an insertion sort on each stride length
+    for (int i = k-2; i >= 0; i--){
+        hSort(arr, n, stridelengths[i]);
     }
-
-    printf("%d", stridelengths[1]);
 }
 
 int main(){
-    int arr[100];
-    shellSort(arr, 1000000000);
+    int list_sizes[5] = {10, 100, 1000, 10000, 100000};
+
+    for (int i = 0; i < 5; i++){
+        int list[list_sizes[i]];
+
+        for (int i = 0; i < list_sizes[i]; i++) {
+            list[i] = rand();
+        }
+        clock_t begin = clock();
+
+        shellSort(list, list_sizes[i]);
+
+        clock_t end = clock();
+        double time_spent = (double)(end-begin) / CLOCKS_PER_SEC;
+
+        printf("Sort took %f seconds to execute on list of size %d\n", time_spent, list_sizes[i]);
+    }
 }
+
